6f8247cf29007344367ef85ad28e53b223b65195
Dawei Wang <wdawei.mis@gmail.com>
2021-07-18 18:21:24 -0500

    Assignment: Caching the Inverse of a Matrix
    The submission contains function makeCacheMatrix and cacheSolve.

---
+++ makeCacheMatrix.R
@@ -0,0 +1,27 @@
+##Q1
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() {x}
+        setInverse <- function(inverse) {inv <<- inverse}
+        getInverse <- function() {inv} ## get the value of inverse
+        list(set = set, get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
+}
+
+## Q2
+cacheSolve <- function(x, ...) {
+        inv<-x$getInverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+       mat<-x$get()
+       inv<-solve(mat,...)
+       x$setInverse(inv)
+       inv
+}
